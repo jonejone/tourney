4f5eca4b79038f263c10302fd7aca3cca9a7622f
Jone Eide <jone@idev.no>
2013-01-07 16:01:58 +0100

    Added some views with tests

--- tourney/tournament/tests.py
+++ tourney/tournament/tests.py
@@ -1,16 +1,39 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.contrib.auth.models import User
+
+from datetime import date
+
+from .models import Tournament, Player
+from .forms import RegistrationForm
+
+class RegisterPlayerTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            'test', 'foo@bar.com', 'foo')
+
+        self.tournament = Tournament.objects.create(
+            name='Some tournament',
+            slug='foo',
+            user=self.user,
+            start_date=date.today())
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def test_post_register_player(self):
+        r = self.client.post(
+            '/registration/',
+            {'name': 'Tester'})
+
+        self.assertEqual(r.status_code, 302)
+
+        self.assertEqual(
+            r['location'],
+            'http://testserver/registration-complete/')
+
+        self.assertEqual(Player.objects.count(), 1)
+
+
+    def test_get_register_player(self):
+        r = self.client.get('/registration/')
+
+        self.assertEqual(r.status_code, 200)
+        self.assertTrue('id_pdga_number' in r.content)
--- tourney/tournament/views.py
+++ tourney/tournament/views.py
@@ -1,1 +1,47 @@
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.conf import settings
+
+from tourney.tournament.models import Tournament
+from .forms import RegistrationForm
+
+def get_settings_tournament():
+    return Tournament.objects.get(
+        id=settings.TOURNAMENT_ID)
+
+
+def players(request):
+    pass
+
+
+def registration(request):
+    tournament = get_settings_tournament()
+
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            p = form.save()
+            return HttpResponseRedirect(
+                '/registration-complete/')
+    else:
+        form = RegistrationForm()
+
+    tmpl_dict = {
+        'tournament': tournament,
+        'form': form,
+    }
+
+    return render(request,
+        'tournament/registration.html', tmpl_dict)
+
+
+def index(request):
+    tournament = get_settings_tournament()
+
+    tmpl_dict = {
+        'tournament': tournament,
+    }
+
+    return render(request,
+        'tournament/index.html', tmpl_dict)
