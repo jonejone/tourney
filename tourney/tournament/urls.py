804e22e0ce0f80efcac678f5c8b2b4443e1793e0
Jone Eide <jone@idev.no>
2013-01-21 17:16:22 +0100

    Added tournament URLs

--- tourney/tournament/urls.py
+++ tourney/tournament/urls.py
@@ -4,11 +4,34 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    (r'^ckeditor/', include('ckeditor.urls')),
 )
 
 urlpatterns += patterns(
     'tourney.tournament.views',
     url(r'^$', 'index', name='tournament-index'),
-    url(r'^registration/$', 'registration', name='tournament-registration'),
+    url(
+        r'^registration/$',
+        'registration',
+        name='tournament-registration'),
+    url(
+        r'^registration-complete/$',
+        'registration_complete',
+        name='tournament-registration-complete'),
+    url(
+        r'^ajax/check-pdga-number/$',
+        'check_pdga_number',
+        name='tournament-ajax-check-pdga-number'),
+
     url(r'^players/$', 'players', name='tournament-players'),
+
+    url(
+        r'^(?P<slug>[-\w]+)/$',
+        'page',
+        name='tournament-page'),
+
+    url(
+        r'^(?P<slug>[-\w]+)/edit/$',
+        'page_edit',
+        name='tournament-page-edit')
 )
