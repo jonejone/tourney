06425c9342fec5d300c4e37ea8d98caada5fb125
Jone Eide <jone@idev.no>
2013-01-23 15:03:38 +0100

    Added filter for navigation pages

--- tourney/tournament/context_processors.py
+++ tourney/tournament/context_processors.py
@@ -3,8 +3,9 @@ def tournament(request):
     context = {}
 
     if request.tournament:
-        pages = request.tournament.tournamentpage_set.order_by(
-            'navigation_position')
+        pages = request.tournament.tournamentpage_set.filter(
+            show_in_navigation=True).order_by(
+                'navigation_position')
 
         context.update({
             'tournament': request.tournament,
