f9f42c0a6afc8397e0165d040fdc73e7a46b876b
Jone Eide <jone@idev.no>
2013-02-05 18:33:56 +0100

    Added serving of MEDIA_ROOT

--- tourney/tournament/urls.py
+++ tourney/tournament/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
@@ -6,7 +7,15 @@ urlpatterns = patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
     (r'^ckeditor/', include('ckeditor.urls')),
+
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+   )
 
 urlpatterns += patterns(
     'tourney.tournament.views',
