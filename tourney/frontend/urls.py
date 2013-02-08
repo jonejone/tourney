7e967a31c36c552167379e652b7276dfc5297765
Jone Eide <jone@idev.no>
2013-02-09 00:05:08 +0100

    Added frontend application

---
+++ tourney/frontend/urls.py
@@ -0,0 +1,24 @@
+from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns(
+    '',
+    url(r'^admin/', include(admin.site.urls)),
+    (r'^ckeditor/', include('ckeditor.urls')),
+
+)
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+   )
+
+
+urlpatterns = patterns(
+    'tourney.frontend.views',
+    url(r'^$', 'index', name='frontend-index'),
+)
