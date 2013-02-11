0c9cdf349a0176ea17dbba2f5a198087537bfb94
Jone Eide <jone@idev.no>
2013-02-11 14:13:29 +0100

    Added S3 util classes for media and static files

---
+++ tourney/s3utils.py
@@ -0,0 +1,21 @@
+from storages.backends.s3boto import S3BotoStorage
+
+
+class StaticRootS3BotoStorage(S3BotoStorage):
+    """
+    Storage for static files.
+    """
+
+    def __init__(self, *args, **kwargs):
+        kwargs['location'] = 'static'
+        super(StaticRootS3BotoStorage, self).__init__(*args, **kwargs)
+
+
+class MediaRootS3BotoStorage(S3BotoStorage):
+    """
+    Storage for uploaded media files.
+    """
+
+    def __init__(self, *args, **kwargs):
+        kwargs['location'] = 'media'
+        super(MediaRootS3BotoStorage, self).__init__(*args, **kwargs)
