7c0b8d8e63b4baaca2eaa82300aa537dfde440de
Jone Eide <jone@idev.no>
2013-02-28 11:50:01 +0100

    Added registration templates and profile view

--- tourney/frontend/urls.py
+++ tourney/frontend/urls.py
@@ -8,6 +8,12 @@ urlpatterns = patterns(
     url(r'^admin/', include(admin.site.urls)),
     (r'^ckeditor/', include('ckeditor.urls')),
 
+    # The welcome to logged in view
+    url(
+        r'^accounts/profile/',
+        'tourney.frontend.views.profile',
+        name='frontend-accounts-profile'),
+
 )
 
 if settings.DEBUG:
--- tourney/frontend/views.py
+++ tourney/frontend/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
-from tourney.tournament.models import Tournament
+from django.contrib.auth.decorators import login_required
+from tourney.tournament.models import Tournament, TournamentAdmin
+
+
+@login_required
+def profile(request):
+    tmpl_dict = {}
+
+    # Find tournaments where user is admin
+    tournaments = [ta.tournament for ta in \
+        request.user.tournamentadmin_set.all()]
+
+    tmpl_dict.update({'tournaments': tournaments})
+
+    # See if we are in a tournament context, use different
+    # base template if that is the case
+    if hasattr(request, 'tournament'):
+        tmpl_dict.update({'extends_tmpl':
+            'tournament/tournament_base.html'})
+    else:
+        tmpl_dict.update({'extends_tmpl':
+            'frontend/base.html'})
+
+    return render(
+        request, 'frontend/accounts/profile.html', tmpl_dict)
+
 
 def index(request):
     t = Tournament.objects.order_by('-start_date')
