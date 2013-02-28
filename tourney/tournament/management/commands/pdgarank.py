9aa7427478db982766a94643665ebb1b49980d2b
Jone Eide <jone@idev.no>
2013-02-28 15:05:59 +0100

    Added new management feature for updating PDGA ratings

--- tourney/tournament/management/commands/pdgarank.py
+++ tourney/tournament/management/commands/pdgarank.py
@@ -1,14 +1,70 @@
 from django.core.management.base import BaseCommand, CommandError
+from optparse import make_option
 
-from tourney.tournament.models import Player
+from tourney.tournament.models import Player, Tournament
 from tourney.tournament.utils.pdga import PDGARanking
 
 
 class Command(BaseCommand):
-    args = '<player_id>'
+    args = ''
     help = 'Updates player PDGA rank'
 
-    def handle(self, *args, **kwargs):
+    option_list = BaseCommand.option_list + (
+        make_option('--updatemissing',
+            action='store_true',
+            dest='updatemissing',
+            default=False,
+            help='Accept new players on waiting list based on available spots'),
+        )
+
+    def updatemissing(self):
+        t = self.tournament
+        o = self.stdout
+        players = t.tournamentplayer_set.filter(
+            player__pdga_number__isnull=False,
+            player__pdga_rating__isnull=True)
+
+        if not players:
+            o.write('No players to update')
+            return
+
+        o.write('Trying to update %i players' \
+            % players.count())
+
+        for player in players:
+            pdga_rank = PDGARanking(player.player.pdga_number)
+
+            if pdga_rank.rating:
+                player.player.pdga_rating = pdga_rank.rating
+                player.player.save()
+
+                o.write('- updated to %s for %s (%s)' % (
+                    pdga_rank.rating,
+                    player.player.name,
+                    player.player.pdga_number))
+            else:
+                o.write('- unable to update %s (%s)' % (
+                    player.player.name,
+                    player.player.pdga_number))
+
+    def validate_tournament(self, *args, **options):
+        # First we must validate tournament
+        try:
+            t_slug = args[0]
+            tournament = Tournament.objects.get(slug=t_slug)
+            self.tournament = tournament
+        except IndexError:
+            raise CommandError('Please enter a tournament slug')
+        except Tournament.DoesNotExist:
+            raise CommandError('Tournament slug not found')
+
+    def handle(self, *args, **options):
+
+        if options['updatemissing']:
+            self.validate_tournament(*args, **options)
+            self.updatemissing()
+            return
+
         # Check if player ID is valid
         try:
             player = Player.objects.get(
@@ -28,7 +84,7 @@ class Command(BaseCommand):
             player.pdga_rating = pdga.rating
             player.save()
             self.stdout.write('Succesfully updated PDGA rating to' +
-                              ' ' + pdga.rating + ' for player ' +
+                              ' ' + str(pdga.rating) + ' for player ' +
                               player.name)
         else:
             self.stdout.write('Unable to find PDGA rating ' +
